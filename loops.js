67d519d2376e67833dc9402acb23ca9179633c45
Christina Dong <christinadongla@yahoo.com>
2019-01-13 06:45:50 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -17,10 +17,10 @@ function whileLoop(n) {
   return "done";
 }
 
-function incrementVariable() {
-i = i + 1;
 
 function doWhileLoop(array) {
+  function incrementVariable() {
+  i = i + 1;
   do {
     array.pop();
   } while (array.length > 0 && incrementVariable());
